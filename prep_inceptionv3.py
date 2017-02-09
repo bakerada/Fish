090d9bd19e652b4da94942ffdc44000fcfca7114
Adam Baker <bakerada@umich.edu>
2017-02-08 20:53:12 -0500

    update inception prep

--- prep_inceptionv3.py
+++ prep_inceptionv3.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def create_directory(directory):
 	if not os.path.isdir(directory):
-		os.path.mkdir(directory)
+		os.mkdir(directory)
 def check_if_exist(directory):
 	if not os.path.isdir(directory):
 		raise ValueError('Directory does not exist')
@@ -14,20 +14,30 @@ def check_if_exist(directory):
 def read_bbox(filename):
 	data = np.genfromtxt(filename,dtype=float)
 	bbox = []
+	if len(data.shape) == 1:
+		return data[1:]
 	for row in data:
 		bbox.append(row[1:])
 	return bbox
 def crop_image(img,bbox,output_dir):
-	img_class = img.split('_')[0]
+	filename = os.path.basename(img)
+	img_class = filename.split('_')[0]
 	class_directory = os.path.join(output_dir,img_class)
 	create_directory(class_directory)
 	image = io.imread(img)
-	for box in bbox:
-		xmin, ymin, xmax, ymax = np.maximum(box[0],0).astype(int),np.maximum(box[1],0).astype(int),
-								 bbox[2].astype(int),bbox[3].astype(int)
+
+	if isinstance(bbox,np.ndarray):
+		xmin, ymin, xmax, ymax = np.maximum(bbox[0],0).astype(int),np.maximum(bbox[1],0).astype(int),bbox[2].astype(int),bbox[3].astype(int)
 		cropped = image[ymin:ymax,xmin:xmax]
-		save_file = os.path.join(class_directory,img)
+		save_file = os.path.join(class_directory,filename)
 		io.imsave(save_file,cropped)
+
+	else:	
+		for box in bbox:
+			xmin, ymin, xmax, ymax = np.maximum(box[0],0).astype(int),np.maximum(box[1],0).astype(int),box[2].astype(int),box[3].astype(int)
+			cropped = image[ymin:ymax,xmin:xmax]
+			save_file = os.path.join(class_directory,filename)
+			io.imsave(save_file,cropped)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -40,18 +50,21 @@ if __name__ == "__main__":
             help = "Directory to save cropped images")
 
     args = parser.parse_args()
-    img_dir = parser.directory
-    label_dir = parser.annotations
-    output_dir = parser.output_dir
+    img_dir = args.directory
+    label_dir = args.annotations
+    output_dir = args.output_dir
 
     check_if_exist(img_dir)
     check_if_exist(label_dir)
     create_directory(output_dir)
 
 
-    images = glob.glob(img_dir + '*.jpg')
-    annotations = glob.glob(label_dir + '*.txt')
+    images = glob.glob(img_dir + '/*.jpg')
+    annotations = glob.glob(label_dir + '/*.txt')
 
     for img in images:
-    	bbox = read_bbox(annotations[img])
+    	base = os.path.basename(img)
+    	filename = base.split('.')[0]
+    	txt = os.path.join(label_dir,'{}_ssd.txt'.format(filename))
+    	bbox = read_bbox(txt)
     	crop_image(img,bbox,output_dir)
